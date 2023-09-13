92d90cf6373dc10aaf4b74576a58d410be481d68
David CM <david.cunillera@dcm.cat>
2023-09-13 18:34:06 +0200

    Last changes before to run things! Hahah you were super fast Zeynep! ;P
    Model design to run and generate a gradient of pollution and constant STcon ;)

--- 3. Coallescent model run.R
+++ 3. Coallescent model run.R
@@ -1,6 +1,4 @@
 
-
-
 # Charge the function to run the coalescent model
 source("H2020_Lattice_expKernel_Jenv_TempMeta.R")
 
@@ -41,9 +39,11 @@ b.ef<-0.5 # The coefficient of change. If 1 we do a direct proportion between th
 J.freshwater<-ceiling((-Jmin+(J.max/(Max_Area^b.ef))*FW_area^b.ef))
 plot(J.freshwater,FW_area) # We plot to observe the curvilinear relationship
 
+# Species tolerances defined in the 2. script
+filter_NOfilter # Filter of species per site. We will use for tolerance
+
 # Other parameters of the model
 id_NOmodule <- rep(1,nrow(nodes_DaFr)) # Modules if we want some sites to belong to the same module. 
-filter_NOfilter <- matrix(nrow = 200, ncol=nrow(nodes_DaFr), data = 1) # Filter of species per site. We will use for tolerance
 pool_200 <- rep(1,200) #rlnorm(n = 200,5,1) # Distribution of the species pool
 Meta_t0 <- matrix(nrow = length(pool_200), ncol =nrow(nodes_DaFr), 1) #Previous Metacommunity (for considering time relevance)
   
@@ -53,11 +53,12 @@ Meta_t0 <- matrix(nrow = length(pool_200), ncol =nrow(nodes_DaFr), 1) #Previous 
 # other dispersal abilities. They push "overall connectivity" towards higher or lower connections.
 summary(as.vector(Dist_Matrix)[-which(as.vector(Dist_Matrix)==10000)])
 dispersal_distances <- c(0.001,0.1,0.5,1,2,3,10)
-  
+
+tic()
 library(doParallel)
-registerDoParallel(cores = detectCores()-2)
-Disp_scenarios <- foreach(loss=1:length(dispersal_distances), .combine=rbind)%dopar%{#%:%
-#                  foreach(dispersal=1:length(dispersal_distances), .combine=rbind)%dopar%{
+registerDoParallel(cores = detectCores()-1)
+Diff_scenarios <- foreach(dispersal=1:length(dispersal_distances), .combine=rbind)%:%
+                  foreach(pollut=1:length(filter_Pollution), .combine=rbind)%dopar%{
 a <- NULL
 #b <- list()
 for (it in 1:10) {
@@ -66,7 +67,7 @@ output <- H2020_Coalescent.and.lottery.exp.Kernel.J_TempMtcom_tempIT(
   m.pool = 0.001,
   Js = J.freshwater,
   id.module = id_NOmodule,
-  filter.env = filter_NOfilter,
+  filter.env = filter_Pollution[[pollut]],
   M.dist = Dist_Matrix,
   D50 = dispersal_distances[dispersal],
   m.max = 1,
@@ -74,24 +75,48 @@ output <- H2020_Coalescent.and.lottery.exp.Kernel.J_TempMtcom_tempIT(
   temp_Metacom = Meta_t0,
   temp_it = 0,
   id.fixed=NULL, D50.fixed=0, m.max.fixed=0, comm.fixed=pool_200,
-  Lottery=F, it=1, prop.dead.by.it=1, id.obs=1:nrow(nodes_DaFr))
+  Lottery=F, it=500, prop.dead.by.it=0.0002, id.obs=1:nrow(nodes_DaFr))
 a <- rbind(a,output[[1]])
 #b[[it]] <- output[[2]]
 }
 resume.out(a)
 }
+toc()
+
+# > <
+Res_nodes_DaFr <- data.frame()
+for (round in 1:((length(Diff_scenarios)/4))) {
+  #Diff_scenarios[[round]][1:9]
+  S_site<- Diff_scenarios[[round]][10:(nrow(nodes_DaFr)+9)]
+  B_site <- Diff_scenarios[[round]][(nrow(nodes_DaFr)+10):((nrow(nodes_DaFr)*2)+9)]
+  #Out$Median[((nrow(nodes_DaFr)*2)+10):length(Out$Median)]
+  
+  if(round<10){Pol_level <- as.character(round)}else{Pol_level <- strsplit(x = as.character(round),split = "")[[1]][2]}
+  if(Pol_level=="0"){Pol_level <- "10"}
+  
+  Result_df <- bind_cols(nodes_DaFr_Pol %>% filter(Pol_Scen==1),
+                         data.frame("D50"=rep(Diff_scenarios[[round]][4],length(S_site)),
+                                    "Pol_Scend_Simulation"=rep(Pol_level,length(S_site)),"S"=S_site,"B"=B_site))
+  Res_nodes_DaFr <- bind_rows(Res_nodes_DaFr,Result_df)
+}
+
+# Checing scenario assignation
+which(
+as.vector(Res_nodes_DaFr %>% mutate("Checking"=Pol_Scen-as.numeric(Pol_Scend_Simulation)) %>% select(Checking))$Checking>0
+)
+
+plot_list <- list()
+for (plottt in 1:10) {
+plot_list[[plottt]] <- Res_nodes_DaFr %>% filter(Pol_Scen==as.character(plottt)) %>% 
+  select(-c(5:14)[-plottt]) %>% rename("Pollution"=colnames(Res_nodes_DaFr %>% dplyr::select(c(5:14)[plottt]))) %>% 
+  ggplot()+geom_boxplot(aes(x=as.factor(Permanence),y=S,fill=Pollution))+
+  facet_grid(.~as.factor(D50))
+  
+}
+grid.arrange(
+plot_list[[1]],plot_list[[2]],plot_list[[3]],plot_list[[4]],plot_list[[5]],
+plot_list[[6]],plot_list[[7]],plot_list[[8]],plot_list[[9]],plot_list[[10]],
+ncol=2)
 
 
-Out$Median[1:9]
-S_site<- Out$Median[10:(nrow(nodes_DaFr)+9)]
-B_site <- Out$Median[(nrow(nodes_DaFr)+10):((nrow(nodes_DaFr)*2)+9)]
 
-Out$Median[((nrow(nodes_DaFr)*2)+10):length(Out$Median)]
-
-Res_nodes_DaFr <- bind_cols(nodes_DaFr,"S"=S_site,"B"=B_site)
-
-Res_nodes_DaFr %>% 
-  ggplot()+geom_point(aes(x=Permanence, y=S,size=weight))+
-  theme_classic()
-
-Res_nodes_DaFr_2.63 <- Res_nodes_DaFr
